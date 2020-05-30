3f12aa72791ebed3829803c105af4aa2d407a4b9
Donghun Park <donghunp1212@gmail.com>
2020-05-31 03:04:46 +0900

    Update UserCode.c

--- OOP/UserCode.c
+++ OOP/UserCode.c
@@ -1,9 +1,14 @@
 #include "stack.h"
 
+/*
+    #define newStackWithValidator(buf, pValidator)   {0, sizeof(buf)/sizeof(int), (buf), pValidator}
+    
+*/
+
 int main(){
     int buf[16];
     Validator v = {validateRange, &{0,9}}; // Funtion addr, Data addr
-    Stack st = newStackWithValidator(buf, &v);
+    Stack st = newStackWithValidator(buf, &v); 
     push(&st, 123);
  
     return 0;
