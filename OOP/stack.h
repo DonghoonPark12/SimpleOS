25597999bad0d23c49eb746b262d51c46ea54820
Donghun Park <donghunp1212@gmail.com>
2020-05-31 01:59:45 +0900

    Update stack.h
    범위 검사 기능을 스택에 추가하지 않고 분리하여 메모리 낭비 방지 및 내용 분리

--- OOP/stack.h
+++ OOP/stack.h
@@ -8,6 +8,11 @@ extern "C" {
 #endif
 
 typedef struct{
+  const int min;
+  const int max;
+} Range;
+  
+typedef struct{
   int top;
   const size_t size;
   int* const pBuf;
@@ -16,8 +21,10 @@ typedef struct{
 bool push(Stack *p, int val);
 bool pop(Stack *p, int *pRet);
 
-#define newStack(buf)  {0, sizeof(buf)/sizeof(int), buf};
+#define newStack(buf)  {0, sizeof(buf)/sizeof(int), (buf)};
 
+#define newStackWithRangeCheck(buf, pRange)   {0, sizeof(buf)/sizeof(int), (buf), pRange}
+  
 #ifdef __cplusplus
 }
 #endif
