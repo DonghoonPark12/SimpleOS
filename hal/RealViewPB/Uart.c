74d9cd98222b451ddfdbbb6c9a8c6c7f80b943fd
Donghun Park <donghunp1212@gmail.com>
2020-06-02 21:33:44 +0900

    Update Uart.c

--- hal/RealViewPB/Uart.c
+++ hal/RealViewPB/Uart.c
@@ -8,4 +8,15 @@ void Hal_uart_init()
 {
   //Enable Uart
   Uart->uartcr.bits.UARTEN = 0;
-  
+  Uart->uartcr.bits.TXE = 1;
+  Uart->uartcr.bits.RXE = 1;
+  Uart->uartcr.bits.UARTEN = 1;
+};
+
+void Hal_uart_put_char(uint8_t ch)
+{
+  while(Uart->uartfr.bits.TXFF); /* Wait until output buffer size 0 */ 
+  Uart->uartdr.all = (ch & 0xFF);
+}
+
+
