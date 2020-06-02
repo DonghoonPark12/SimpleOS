1f011627e97af8e6a955190b97813a02c8ddbc8f
Donghun Park <donghunp1212@gmail.com>
2020-06-02 21:37:59 +0900

    Update main.c

--- boot/main.c
+++ boot/main.c
@@ -1,5 +1,19 @@
 #include "stdint.h"
+#include "../hal/HalUart.h"
+
+static void Hw_init(void);
 
 void main(){
+  Hw_init();
   
+  uint32_t i = 100;
+  while(i--)
+  {
+    Hal_uart_put_char('N');  
+  }
+}
+
+static void Hw_init(void)
+{
+    Hal_Uart_init();
 }
