34bb9b9e72c7e0e369a98a421556ea4550529d3e
Donghun Park <donghunp1212@gmail.com>
2021-01-10 15:48:15 +0900

    Update stdlib.c

--- lib/stdlib.c
+++ lib/stdlib.c
@@ -11,7 +11,7 @@
 
 void delay(uint32_t ms)
 {
-    uint32_t goal = Hal_timer_get_1ms_counter() + ms;
+    uint32_t goal = Hal_timer_get_1ms_counter() + ms; // 현재 값 + 지연 시키고 싶은 시간
     
     while(goal != Hal_timer_get_1ms_counter());
 }
