4c864cd04e1d50642d385263a906e0505937ae10
Donghun Park <donghunp1212@gmail.com>
2021-01-10 15:33:50 +0900

    Create stdlib.h

---
+++ lib/stdlib.h
@@ -0,0 +1,13 @@
+/*
+ * stdlib.h
+ *
+ *  Created on: Jan 10, 2021
+ *      Author: dhpark
+ */
+ 
+#ifndef LIB_STDLIB_H_
+#define LIB_STDLIB_H_
+
+void delay(uint32_t ms);
+
+#endif /* LIB_STDLIB_H_ */
