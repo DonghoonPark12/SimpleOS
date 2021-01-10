104ed7dba62b3c1b7ed1284a9198481c0cdc4ef1
Donghun Park <donghunp1212@gmail.com>
2021-01-10 13:14:10 +0900

    Create memio.h

---
+++ Include/memio.h
@@ -0,0 +1,14 @@
+/*
+ * memio.h
+ *
+ *  Created on: Jan 01, 2021
+ *      Author: dhpark
+ */
+ 
+#ifndef INCLUDE_MEMIO_H_
+#define INCLUDE_MEMIO_H_
+
+#define SET_BIT(p,n) ((p) |=  (1 << (n)))
+#define CLR_BIT(p,n) ((p) &= ~(1 << (n)))
+
+#endif /* INCLUDE_MEMIO_H_ */
