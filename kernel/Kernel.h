3c185e04a6a2a1bd9d2cc45da0c4ab5c6f5c1771
Donghun Park <donghunp1212@gmail.com>
2021-01-17 15:44:05 +0900

    Create Kernel.h

---
+++ kernel/Kernel.h
@@ -0,0 +1,16 @@
+/*
+ * Kernel.h
+ *
+ *  Created on: Jan 17, 2021
+ *      Author: dhpark
+ */
+
+#ifndef KERNEL_KERNEL_H_
+#define KERNEL_KERNEL_H_
+
+#include "task.h"
+
+void Kernel_start(void);
+void Kernel_yield(void);
+
+#endif /* KERNEL_KERNEL_H_ */
